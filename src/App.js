af853e8d98ae623efe53e7dfea8e90ef6e30a1f2
UladzimirKhilko <vladimirhilko@gmail.com>
2021-09-15 21:27:03 +0300

    project complete

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Heder } from "./layout/Heder";
+import {Footer} from "./layout/Footer"
+import {Main} from "./layout/Main";
+
+function App() {
+  return (
+    <>
+      <Heder />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
